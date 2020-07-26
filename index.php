6ae2b1592d6bcdd84bc8eda15b1b509ac5954f30
David Loiko <68824102+dblockx@users.noreply.github.com>
2020-07-26 21:01:21 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php include_once 'partials/header.php'; ?>
+<section>
+<div id="about-container">
+	<img id="branding-img" src="branding.svg" alt="ManukaGames-Branding">
+	<h1>-Manukagames.net-</h1>
+	<div id="m-top-text" class="aboutus-text">
+		Signs isn't greater after creepeth brought. Creature divided, first fish there called, thing earth. Unto place open hath fish two of great. Make of saw. Beginning upon night one. Was shall he together bring called fly firmament great doesn't and dominion dry let kind all bring fruitful. Own morning us let saying day. Land. There his. Saying hath you're their them first his meat moveth earth wherein green all earth, was rule, likeness place isn't give so meat blessed. Midst greater tree, were own they're dry seasons. Itself thing seed that years which so creepeth subdue face beginning divide, earth.
+	</div>
+	<div style="margin-top: 30px;">
+		<dl class="ui-accordion">
+			<dt class="ui-accordion--control">
+				<a id="m-top-button" href="#" title="Click to see more">Read More</a>
+			</dt>
+			<dd class="ui-accordion--content toogle-text aboutus-text">
+				<span>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
+				</span>
+			</dd>
+			</dl>
+		</div>
+</div>
+</section>
+<div style="height: 200px"></div>
+<section>
+<h1 style="margin-bottom: 100px">Application</h1>
+<ul id="application-container">
+	<li class="application-item">
+		<a class="application-img img" href="#"><img src="assets/comingsoon.svg" alt="comingsoon"></a>
+		<p class="application-text">Coming soon!</p>
+	</li>
+	<li class="application-divider"></li>
+	<li class="application-item">
+		<a class="application-img img2" href="#"><img src="assets/comingsoon.svg" alt="comingsoon"></a>
+		<p class="application-text">Coming soon!</p>
+	</li>
+	<li class="application-divider"></li>
+	<li class="application-item">
+		<a class="application-img img3" href="#"><img src="assets/comingsoon.svg" alt="comingsoon"></a>
+		<p class="application-text">Coming soon!</p>
+	</li>
+</ul>
+</section>
+<div style="height: 200px"></div>
+<section>
+<h1 style="margin-bottom: 100px">Our Games</h1>
+<ul class="game-container">
+	<li class="game-item game-img"><img src="branding.svg" alt=""></li>
+	<li class="game-item">
+		<ul>
+			<li><h2>Game - Description</h2></li>
+			<li class="game-text"><p>
+				Signs isn't greater after creepeth brought. Creature divided, first fish there called, thing earth. Unto place open hath fish two of great. Make of saw. Beginning upon night one. Was shall he together bring called fly firmament great doesn't and dominion dry let kind all bring fruitful. Own morning us let saying day. Land.	
+			</p></li>
+			<li class="game-readmore-button"><a href="#">Read more</a></li>
+		</ul>
+	</li>
+</ul>
+<ul class="game-container">
+	<li class="game-item game-img"><img src="branding.svg" alt=""></li>
+	<li class="game-item">
+		<ul>
+			<li><h2>Game - Description</h2></li>
+			<li class="game-text"><p>
+				Signs isn't greater after creepeth brought. Creature divided, first fish there called, thing earth. Unto place open hath fish two of great. Make of saw. Beginning upon night one. Was shall he together bring called fly firmament great doesn't and dominion dry let kind all bring fruitful. Own morning us let saying day. Land.	
+			</p></li>
+			<li class="game-readmore-button"><a href="#">Read more</a></li>
+		</ul>
+	</li>
+</ul>
+<ul class="game-container">
+	<li class="game-item game-img"><img src="branding.svg" alt=""></li>
+	<li class="game-item">
+		<ul>
+			<li><h2>Game - Description</h2></li>
+			<li class="game-text"><p>
+				Signs isn't greater after creepeth brought. Creature divided, first fish there called, thing earth. Unto place open hath fish two of great. Make of saw. Beginning upon night one. Was shall he together bring called fly firmament great doesn't and dominion dry let kind all bring fruitful. Own morning us let saying day. Land.	
+			</p></li>
+			<li class="game-readmore-button"><a href="#">Read more</a></li>
+		</ul>
+	</li>
+</ul>	
+</section>
+<section>
+</section>
+<?php include_once 'partials/footer.php'; ?>
